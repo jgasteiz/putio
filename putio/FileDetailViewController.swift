442287ebc97a70253d8805ea3a49b89be1a08a45
Javi Manzano <javi.manzano.oller@gmail.com>
2015-11-01 19:28:48 +0000

    Add detail and webview view controllers

---
+++ putio/FileDetailViewController.swift
@@ -0,0 +1,64 @@
+//
+//  FileViewController.swift
+//  putio
+//
+//  Created by Javi Manzano on 01/11/2015.
+//  Copyright © 2015 Javi Manzano. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class FileDetailViewController: UIViewController {
+    
+    var file: File? = nil
+    
+    let webViewSegueId = "showWeb"
+    let mediaSegueId = "showMedia"
+    
+    @IBOutlet weak var fileName: UILabel!
+    @IBOutlet weak var fileSize: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.title = self.file!.getName()
+        
+        fileName.text = self.file!.getName()
+        fileSize.text = self.file!.getSize()
+        
+        if file!.isVideo() {
+            playButton.setTitle("Play Video", forState: UIControlState.Normal)
+        } else if file!.isAudio() {
+            playButton.setTitle("Play Audio", forState: UIControlState.Normal)
+        } else {
+            playButton.setTitle("View in WebView", forState: UIControlState.Normal)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == webViewSegueId {
+            let controller = segue.destinationViewController as! WebViewViewController
+            controller.file = file
+        }
+    }
+    
+    @IBAction func viewFile(sender: AnyObject) {
+        if file!.isVideo() || file!.isAudio() {
+            let secondViewController = self.storyboard?.instantiateViewControllerWithIdentifier("MediaViewController") as! AVPlayerViewController
+            secondViewController.player = AVPlayer(URL: file!.getDownloadURL())
+            self.presentViewController(secondViewController, animated: true, completion: nil)
+        } else {
+            self.performSegueWithIdentifier(webViewSegueId, sender: sender)
+        }
+    }
+    
+}
---
+++ putio/WebViewViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FileViewController.swift
+//  putio
+//
+//  Created by Javi Manzano on 31/10/2015.
+//  Copyright © 2015 Javi Manzano. All rights reserved.
+//
+
+import UIKit
+
+class WebViewViewController: UIViewController {
+    
+    var file: File? = nil
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.title = self.file!.getName()
+        
+        let url : NSURL! = file!.getDownloadURL()
+        webView.loadRequest(NSURLRequest(URL: url))
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
