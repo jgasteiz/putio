fa081c0105399763dcb3f104a49220a97c73e7ef
Javi Manzano <javi.manzano.oller@gmail.com>
2015-11-03 23:51:05 +0000

    Add logic to download/remove downloads from local file storage

--- putio/FileDetailViewController.swift
+++ putio/FileDetailViewController.swift
@@ -17,21 +17,28 @@ class FileDetailViewController: UIViewController {
     let webViewSegueId = "showWeb"
     let mediaSegueId = "showMedia"
     
+    // Get an instance of the file manager
+    let fileManager = NSFileManager.defaultManager()
+    let documentsDirectory = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+    var offlineFileURL: NSURL?
+    
     @IBOutlet weak var fileName: UILabel!
     @IBOutlet weak var fileSize: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var downloadButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.navigationItem.title = self.file!.getName()
+        self.navigationItem.title = file!.getName()
         
-        fileName.text = self.file!.getName()
-        fileSize.text = self.file!.getSize()
+        fileName.text = file!.getName()
+        fileSize.text = file!.getSize()
         
-        if let thumbnailURL = NSURL(string: self.file!.getThumbnail()) {
+        if let thumbnailURL = NSURL(string: file!.getThumbnail()) {
             if let data = NSData(contentsOfURL: thumbnailURL){
                 imageView.contentMode = UIViewContentMode.ScaleAspectFit
                 imageView.image = UIImage(data: data)
@@ -44,6 +51,25 @@ class FileDetailViewController: UIViewController {
             playButton.setTitle("Play Audio", forState: UIControlState.Normal)
         } else {
             playButton.setTitle("View in WebView", forState: UIControlState.Normal)
+        }
+        
+        // Initialise the `offline file url`
+        let offlineMediaDirectory = documentsDirectory.URLByAppendingPathComponent("Offline", isDirectory: true)
+        // Try to create it if it doesn't exist.
+        do {
+            try fileManager.createDirectoryAtURL(offlineMediaDirectory, withIntermediateDirectories: false, attributes: nil)
+        } catch {
+            print("Failed creating downloads directory")
+        }
+        offlineFileURL = offlineMediaDirectory.URLByAppendingPathComponent(file!.getOfflineFileName())
+        
+        // If the file is downloaded, remove the "Download" button.
+        if isFileDownloaded() {
+            downloadButton.hidden = true
+        }
+        // Otherwise remove the "Delete download" button.
+        else {
+            deleteButton.hidden = true
         }
     }
     
@@ -69,4 +95,45 @@ class FileDetailViewController: UIViewController {
         }
     }
     
+    @IBAction func downloadFile(sender: AnyObject) {
+        // If the file is downloaded already, don't continue.
+        if isFileDownloaded() {
+            notifyUser("This file has been downloaded already.")
+            return
+        }
+        
+        // Save file.
+        // TODO: save the file using an async task.
+        let fileData = NSData(contentsOfURL: file!.getDownloadURL())!
+        fileData.writeToURL(offlineFileURL!, atomically: true)
+        downloadButton.hidden = true
+        deleteButton.hidden = false
+    }
+    
+    @IBAction func deleteFile(sender: AnyObject) {
+        // If the file hasn't been downloaded yet, don't continue.
+        if !isFileDownloaded() {
+            notifyUser("This file hasn't been downloaded yet.")
+            return
+        }
+        
+        do {
+            try fileManager.removeItemAtURL(offlineFileURL!)
+        } catch {
+            print("There was an error deleting \(file!.getOfflineFileName())")
+        }
+        deleteButton.hidden = true
+        downloadButton.hidden = false
+    }
+    
+    func isFileDownloaded() -> Bool {
+        return fileManager.fileExistsAtPath(offlineFileURL!.path!)
+    }
+    
+    func notifyUser(message: String) -> Void {
+        // Show error message
+        let alertController = UIAlertController(title: "Ooops", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+        presentViewController(alertController, animated: true, completion: nil)
+    }
 }
