276abaec8433dcfd30de6b8d74e0de8f51690263
Javi Manzano <javi.manzano.oller@gmail.com>
2016-09-03 19:44:53 +0100

    Add podfile and alamofire

---
+++ Pods/Target Support Files/Pods-putio/Pods-putio-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_putio : NSObject
+@end
+@implementation PodsDummy_Pods_putio
+@end
---
+++ Pods/Target Support Files/Pods-putio/Pods-putio-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_putioVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_putioVersionString[];
+
